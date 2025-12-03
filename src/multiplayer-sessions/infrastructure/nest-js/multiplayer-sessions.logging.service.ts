88e1db0444c7934398a6267c1948c4c3abbafa1e
Franku03 <franklinquintana2003@gmail.com>
2025-12-02 20:54:15 -0400

    WebSocket API con salas funcional con un servicio de Loggeo, faltan la definicion de los eventos y las validaciones de JWT para cuando llegue el modulo de Auth

---
+++ src/multiplayer-sessions/infrastructure/nest-js/multiplayer-sessions.logging.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+import { SessionRoles } from './enums/session-roles.enum';
+import { WsException } from '@nestjs/websockets';
+
+interface ConnectedClients {
+
+    [id: string]: {
+        socket: Socket,
+        nickname: string,
+        roomPin: string
+        role: SessionRoles,
+    }; 
+}
+
+@Injectable()
+export class MultiplayerSessionsService {
+
+    private availableRooms: Map<string, ConnectedClients> = new Map<string, ConnectedClients>();
+
+    registerRoom( client: Socket ){
+
+
+        const roomPin = client.handshake.headers.pin as string;
+
+        this.availableRooms.set( roomPin, {} );
+
+    } 
+
+
+    registerClient( client: Socket ){
+
+        const nickname = client.handshake.headers.nickname as string;
+
+        const roomPin = client.handshake.headers.pin as string;
+
+        const role = client.handshake.headers.role as SessionRoles;
+
+        const room = this.getRoom( roomPin );
+
+        room[ client.id ] = {
+            socket: client,
+            roomPin: roomPin,
+            role: role,
+            nickname: nickname,
+        };
+
+    } 
+
+
+    removeClient( roomPin: string, clientId: string){
+
+        const room = this.availableRooms.get( roomPin );
+
+        if(!room)
+            this.roomDoesNotExist( roomPin );
+
+        delete room[ clientId ];
+    }
+
+    getConnectedClients(): ([ string, ConnectedClients])[] {
+        return [ ...this.availableRooms ]; 
+    }
+
+    // getUserFullNameBySocketId( socketId: string ){
+    //     return this.connectedClients[ socketId ]
+    // }
+
+    private getRoom( roomPin: string ): ConnectedClients {
+        const room = this.availableRooms.get( roomPin );
+
+        if(!room)
+            this.roomDoesNotExist( roomPin );
+
+        return room;;
+    }
+
+    private  roomDoesNotExist( arg: any ): never {
+        throw new WsException(`La sala con PIN ${arg} a unirse NO Existe`);
+    }
+}
