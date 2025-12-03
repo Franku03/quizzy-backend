2541f1de8f245a6d9ac80155f8386ce06edc642d
Franku03 <franklinquintana2003@gmail.com>
2025-12-03 03:24:10 -0400

    Avances en el CreateSessionCommand, Injectable() en UUID-generator

--- src/multiplayer-sessions/application/commands/create-session.handler.ts
+++ src/multiplayer-sessions/application/commands/create-session.handler.ts
@@ -6,8 +6,16 @@ import { RepositoryName } from "src/database/infrastructure/catalogs/repository.
 import { InMemorySessionRepository } from "src/multiplayer-sessions/infrastructure/repositories/InMemorySessionRepository";
 
 import type { IKahootRepository } from "src/kahoots/domain/ports/IKahootRepository";
+import type { IGeneratePinService } from "src/multiplayer-sessions/domain/domain-services";
+import type { IdGenerator } from "src/core/application/idgenerator/id.generator";
+
 import { KahootId } from "src/core/domain/shared-value-objects/id-objects/kahoot.id";
 import { MultiplayerSessionFactory } from "src/multiplayer-sessions/domain/factories/multiplayer-session.factory";
+import { UuidGenerator } from "src/core/infrastructure/event-buses/idgenerator/uuid-generator";
+import { UserId } from "src/core/domain/shared-value-objects/id-objects/user.id";
+import { CryptoGeneratePinService } from "src/multiplayer-sessions/infrastructure/adapters/crypto-generate-pin";
+import { SlideId } from '../../../core/domain/shared-value-objects/id-objects/kahoot.slide.id';
+import { MultiplayerSessionId } from "src/core/domain/shared-value-objects/id-objects/multiplayer-session.id";
 
 
 @CommandHandler( CreateSessionCommand )
@@ -19,28 +27,56 @@ export class CreateSessionHandler implements ICommandHandler<CreateSessionComman
 
         @Inject( RepositoryName.Kahoot )
         private readonly kahootRepository: IKahootRepository,
+
+        @Inject( UuidGenerator )
+        private readonly IdGenerator: IdGenerator<string>,
+
+        @Inject( CryptoGeneratePinService)
+        private readonly sessionPinGenerator: IGeneratePinService
     ){}
 
     async execute(command: CreateSessionCommand): Promise<void> {
 
-        // // Cargamos el agregado kahoot desde el repositorio
-        // const tempKahootId = new KahootId( command.kahootId );
+        // Cargamos el agregado kahoot desde el repositorio
+        const tempKahootId = new KahootId( command.kahootId );
 
-        // const kahootOpt = await this.kahootRepository.findKahootById( tempKahootId );
+        const kahootOpt = await this.kahootRepository.findKahootById( tempKahootId );
 
-        // if( !kahootOpt.hasValue() )
-        //     throw new Error("El Kahoot solicitado no existe"),
+        if( !kahootOpt.hasValue() )
+            throw new Error("El Kahoot solicitado no existe");
 
+        const kahoot = kahootOpt.getValue()
 
+        const slideId = new SlideId( kahoot.getNextSlideSnapshotByIndex()?.id! )
 
-        // // const session = MultiplayerSessionFactory.createMultiplayerSession({
-            
+        const kahootInfo = {
+            kahootId: kahoot.id,
+            firstSlideId: slideId,
+            slidesNumber: kahoot.hasHowManySlides(), // ! Algo me dice que hay un problema con el numero de slidesTotales y el progreso
+        }
 
-        // // })
+        // Creamos el id de la sesion y reconstruimos el VO del id del user host
+        const sessionIdString = await this.IdGenerator.generateId();
+        const sessionId = new MultiplayerSessionId( sessionIdString );
 
-        // // await this.sessionRepository.save({});
+        const hostId = new UserId( command.hostId );
 
-        // return new Promise()
+        // Generamos el Pin de la sesion
+        const pin = await this.sessionPinGenerator.generateUniquePin();
+
+        const session = MultiplayerSessionFactory.createMultiplayerSession(
+            kahootInfo,
+            hostId,
+            sessionId,
+            pin
+        )
+
+        await this.sessionRepository.save({
+            session,
+            kahoot,
+            lastActivity: 0 // Luego se actualizara al guardarse
+        });
+
     }
 
 }
--- src/multiplayer-sessions/infrastructure/nest-js/multiplayer-sessions.controller.ts
+++ src/multiplayer-sessions/infrastructure/nest-js/multiplayer-sessions.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateSessionDto } from './dtos/create-session.dto';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CreateSessionCommand } from 'src/multiplayer-sessions/application/commands/create-session.command';
 
 @Controller('multiplayer-sessions')
 export class MultiplayerSessionsController {
@@ -10,12 +11,13 @@ export class MultiplayerSessionsController {
     private readonly queryBus: QueryBus,
   ){}
 
-
+  // TODO: Agregar obtencion del ID del usuario a traves del JWT por los headers
   @Post()
-  createSession(
+  async createSession(
     @Body() createSessionDto: CreateSessionDto,
+    // TODO: @GetUser('id') userId: string,
   ) {
-    // await this.commandBus.execute( new CreateSessionCommand);
+      await this.commandBus.execute( new CreateSessionCommand( createSessionDto.kahootId, createSessionDto.userId ) );
   }
 
 
