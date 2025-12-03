7805d5f5377d1f45fa5b4f58133972187d3dbb26
kufatty <gekufatty.22@est.ucab.edu.ve>
2025-12-02 23:09:16 -0400

    evaluation service updated, time-details now has iso

--- src/solo-attempts/domain/domain-services/evaluation.service.ts
+++ src/solo-attempts/domain/domain-services/evaluation.service.ts
@@ -2,6 +2,7 @@ import { SoloAttempt } from "../aggregates/attempt";
 import { Kahoot } from "src/kahoots/domain/aggregates/kahoot";
 import { Submission } from "src/core/domain/shared-value-objects/parameter-objects/parameter.object.submission";
 import { AttemptStatus } from "../value-objects/attempt.status";
+import { SlideSnapshot } from "src/core/domain/snapshots/snapshot.slide";
 
 // Service responsible for evaluating a player's answer submission
 // within a solo Kahoot attempt.
@@ -11,34 +12,41 @@ import { AttemptStatus } from "../value-objects/attempt.status";
 // It ensures that scoring rules are applied and the game state advances correctly.
 export class SoloAttemptEvaluationService {
 
-  private attempt: SoloAttempt;
-  private kahoot: Kahoot;
-
   // We inject the aggregates required for evaluation. This ensures the service 
-  // has the full context of the current game state and the rules (Kahoot).
+  // We also receive the submission parameter object encapsulating the player's answers.
   // These are injected by the application layer. Which loads the aggregates from the repositories.
-  constructor(attempt: SoloAttempt, kahoot: Kahoot) {
-    this.attempt = attempt;
-    this.kahoot = kahoot;
-  }
+  public evaluate(kahoot: Kahoot, attempt: SoloAttempt, submission: Submission): void {
 
-  // Evaluates a player's submission against the rules of the Kahoot.
-  // It updates the attempt with the result (correct/incorrect, points earned)
-  // and determines if the attempt should be marked as completed.
-  public evaluate(submission: Submission): void {
-
-    // Cross-Service Invariant Check:
+    // Cross-Aggregate Invariant Checks:
     
     // We make sure the kahoot and attempt correspond.
-    if (!this.attempt.kahootId.equals(this.kahoot.id)) {
+    if (!attempt.kahootId.equals(kahoot.id)) {
       throw new Error("The attempt does not correspond to the provided Kahoot.");
     }
+
+    // We check that the slide being answered is the next one in the attempt's progress.
+    const slide_id_of_new_answer = submission.getSlideId();
+    const position_of_next_slide_in_attempt = attempt.progress.questionsAnswered;
+    const slide_snapshot_of_new_answer = kahoot.getSlideSnapshotById(slide_id_of_new_answer); 
+    if (slide_snapshot_of_new_answer === null) {
+      throw new Error("The slide being answered does not exist in the Kahoot.");
+    }
+    else if (slide_snapshot_of_new_answer.position !== position_of_next_slide_in_attempt) {
+      throw new Error("The slide being answered is not the next one in the attempt's progress.");
+    }
+
+    // If the kahoot is in draft mode you can't continue playing 
+    if (kahoot.isDraft()){
+      throw new Error("Cannot evaluate answers for an attempt on a Kahoot that is in draft mode.");
+    }
+
+    // The other invariant checks are aggregate-only and will be done internally.
 
     // We delegate the calculation of correctness and points to the Kahoot aggregate.
     // The Kahoot knows the correct options and the scoring strategy.
     // It will return a Result parameter object encapsulating the outcome 
     // and data snapshots of the question and answers.
-    const result = this.kahoot.evaluateAnswer(submission);
+    const result = kahoot.evaluateAnswer(submission);
 
     // We register the result into the player's attempt. This records the answer data and snapshots,
     // updates the total score, audits timestamps, and updates the progress (questions answered).
@@ -47,6 +55,7 @@ export class SoloAttemptEvaluationService {
     // Invariants checks and state updates are all encapsulated within the aggregate as well. 
     // It all originates from this aggregate method. 
     // (Which also calls other aggregate methods when needed obviously).
-    this.attempt.registerAnswer(result);
+    attempt.registerAnswer(result);
+
   }
 }
