d7595db439990ca62984991bd057dbf5ab50b930
Luis <luisalejandroochoa30@gmail.com>
2025-12-03 16:52:09 -0400

    arreglando la entity de kahoot (schema)

--- src/kahoots/application/ports/i-kahoot.read.mapper.ts
+++ src/kahoots/application/ports/i-kahoot.read.mapper.ts
@@ -1,5 +1,5 @@
 import { KahootReadModel } from '../queries/read-model/kahoot.response.read.model';
-import { KahootMongoInput } from 'src/kahoots/infrastructure/adapters/querys/output/kahoot.read.model.mapper';
+import { KahootMongoInput } from 'src/database/infrastructure/mongo/entities/kahoots.schema';
 
 export interface IKahootReadResponseMapper {
     mapToReadModel(kahootData: KahootMongoInput): KahootReadModel;
