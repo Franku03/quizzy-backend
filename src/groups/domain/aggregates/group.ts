89faa86d9352ebe5b5289df5a6654d80b79f4f98
Sergio <sergiorod1985@gmail.com>
2026-01-13 21:16:52 -0400

    Implementando validaciones faltantes por repositorio de usuarios

--- src/groups/domain/aggregates/group.ts
+++ src/groups/domain/aggregates/group.ts
@@ -18,7 +18,7 @@ import { AttemptId } from 'src/core/domain/shared-value-objects/id-objects/singl
 import { DomainEvent } from 'src/core/domain/abstractions/domain-event';
 import { KahootAssignedToGroupEvent } from 'src/core/domain/domain-events/kahoot-assigned-to-group.event';
 
-//pending: revisar
+
 import { GroupMemberRole } from '../value-objects/group.member.role';
 
 
@@ -138,7 +138,6 @@ export class Group extends AggregateRoot<GroupProps, GroupId> {
       throw new Error("La invitación no es válida.");
     }
 
-    //pending: revisar
     this.properties.members.push(new GroupMember({ userId: userToJoin, role: new Role(GroupMemberRole.MEMBER), joinedAt: new Date() }, userToJoin));
 
   }
@@ -263,8 +262,6 @@ export class Group extends AggregateRoot<GroupProps, GroupId> {
     if (!this.isAdmin(requesterId)) {
       throw new Error("Solo el admin del grupo puede eliminar el grupo.");
     }
-
-    //pending: implementar la eliminacion del grupo (mediante repositorio)
   }
 
   public isAdmin(userId: UserId): boolean {
